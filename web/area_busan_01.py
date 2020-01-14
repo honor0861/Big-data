a79610a34ad72eee6e9f64c4c6b1f2674a284e38
honor0861 <flare0861@naver.com>
2020-01-14 20:05:06 +0900

    Web projects

--- web/area_busan_01.py
+++ web/area_busan_01.py
@@ -5,10 +5,14 @@ conn = pymongo.MongoClient('192.168.99.100', 32766)
 db = conn.get_database("area1")
 coll = db.get_collection("table")
 
-f = open('C:\\Users\\admin\\Desktop\\python_projects\\area\\busan.csv','r')
+f = open('C:\\Users\\admin\\Desktop\\python_projects\\area\\busan1.csv','r')
 rdr = csv.reader(f)
 next(rdr, None) # 컬럼 skip
 
+edu = list()
+is_cctv = list()
+num_cctv = list()
+load_
 for line in rdr:
     print(line[0])
     # print(type(line))
